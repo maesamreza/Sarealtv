fa266be5092d335475c8655bafe4e6a68e3accd7
irfan-umer <iirfanumer@gmail.com>
2022-11-08 20:23:57 +0500

    new by Irfan

--- database/migrations/2022_11_07_092050_create_media_bookmarks_table.php
+++ database/migrations/2022_11_07_092050_create_media_bookmarks_table.php
@@ -21,6 +21,8 @@ return new class extends Migration
             $table->unsignedBigInteger('client_media_id');
             $table->foreign('client_media_id')->references('id')->on('client_media')->onDelete('cascade');
             $table->unique(['client_id','client_media_id']);
+            $table->unsignedBigInteger('bookmark_list_id');
+            $table->foreign('bookmark_list_id')->references('id')->on('bookmark_lists')->onDelete('cascade');
             $table->timestamps();
         });
     }
