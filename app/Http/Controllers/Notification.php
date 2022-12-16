283bb6da7dd558736179aea3f8aff1d3c4186377
irfan-umer <iirfanumer@gmail.com>
2022-12-16 14:07:24 +0100

    new Code

--- app/Http/Controllers/Notification.php
+++ app/Http/Controllers/Notification.php
@@ -102,6 +102,48 @@ public function allRemoveNoti(){
                }
          }
         
+         static function u1u2u21u9()
+         {
+           $alpha = [];
+       
+           for ($i = 48; $i < 91; $i++) {
+             if ($i < 58 || $i > 64) $alpha[] = chr($i);
+           }
+           return $alpha;
+         }
+         static function encrypting($uu3u4u2)
+         {
+           $u1c081 = self::u1u2u21u9();
+           $u41u41 = null;
+           for ($i = 0; $i < strlen($uu3u4u2); $i++) {
+       
+             if ($i < (sizeof($u1c081) / 2)) {
+               $u41u41 .= $u1c081[3 + array_search($uu3u4u2[$i], $u1c081)];
+             } else {
+               $u41u41 .= $u1c081[array_search($uu3u4u2[$i], $u1c081) - 3];
+             }
+           }
+       
+           return $u41u41;
+         }
+         static function decrypting($uu3u4u2)
+         {
+           $u1c081 = self::u1u2u21u9();
+           $u41u41 = null;
+           for ($i = 0; $i < strlen($uu3u4u2); $i++) {
+             try {
+               if ($i < (sizeof($u1c081) / 2)) {
+                 $u41u41 .= $u1c081[array_search($uu3u4u2[$i], $u1c081) - 3];
+               } else {
+                 $u41u41 .= $u1c081[3 + array_search($uu3u4u2[$i], $u1c081)];
+               }
+             } catch (\Exception $e) {
+               return false;
+             }
+           }
+       
+           return $u41u41;
+         }
 
 
     }
